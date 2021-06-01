274e672b49d83d51ef72a65abd84e661041afe90
Tim Morgan <git@timothymorgan.info>
2021-06-01 11:45:27 -0700

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,19 @@
+echo "* Hard reset of deploy branch"
 pushd deploy || exit
 git fetch
+git checkout gh-pages
 git reset --hard origin/gh-pages
 popd || exit
 
+echo "* Compiling"
 yarn build
+echo "* Copying to deploy branch"
 rsync -rv --delete --exclude=".git" --force dist/ deploy/
 
 pushd deploy || exit
+echo "* Committing deploy"
 git add -A
 git commit -m "Deploy by $USER"
+echo "* Pushing deploy"
 git push
 popd || exit
