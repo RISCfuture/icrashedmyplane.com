c219cf874bafbae5f512ef2434f76a258473a802
Tim Morgan <github@timothymorgan.info>
2024-03-09 19:27:20 -0800

    Vue 3, Vite, Vitest

---
+++ cypress/support/commands.ts
@@ -0,0 +1,44 @@
+/// <reference types="cypress" />
+/// <reference types="@testing-library/cypress" />
+/// <reference types="./commands" />
+// ***********************************************
+// This example commands.ts shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+
+import '@testing-library/cypress/add-commands'
+import { Response as ResponseModel } from '@/models/response'
+import ResponseTraverser, { beyondEndNode } from '../../src/models/response/traverser'
+import { compact, isUndefined } from 'lodash-es'
+import { endNode, isQuestionResponseNode } from '../../src/models/response/answer'
+
+Cypress.Commands.add('runResponse', (response: ResponseModel) => {
+  new ResponseTraverser(response).traverse({
+    visitQuestion(question, answerNode) {
+      if (isUndefined(answerNode) || answerNode === endNode || answerNode === beyondEndNode) {
+        return true
+      }
+
+      if (isQuestionResponseNode(answerNode)) {
+        const chosenOptions = compact(
+          answerNode.nodes.map((next, i) => (next ? question.options[i] : null))
+        )
+
+        if (question.multi) {
+           
+          chosenOptions.forEach((option) => cy.findByTestId(option.identifier).click())
+          cy.findByRole('button').click()
+        } else {
+          cy.findByTestId(chosenOptions[0].identifier).click()
+        }
+      }
+
+      return true
+    }
+  })
+})
--- src/views/Container.vue
+++ src/views/Container.vue
@@ -1,114 +1,92 @@
 <template>
   <div id="container">
-    <transition
-      appear
-      name="in-move-left-out-move-left"
-      mode="out-in"
-    >
+    <transition-group appear name="in-move-left-out-move-left" mode="out-in">
       <welcome v-if="showWelcome" />
-      <question
-        v-if="showQuestion"
-        :prompt="nextPrompt"
-      />
+      <question v-if="showQuestion && nextPrompt" :prompt="nextPrompt" />
       <finished v-if="showFinished" />
-    </transition>
+    </transition-group>
   </div>
 </template>
 
-<script lang="ts">
-  import Vue from 'vue'
-  import Component from 'vue-class-component'
-  import { Getter } from 'vuex-class'
-  import { find, isNil } from 'lodash-es'
-  import Welcome from '@/views/Welcome.vue'
-  import surveyOrder from '@/data/surveyOrder'
-  import Response from '@/models/response'
-  import Finished from '@/views/Finished.vue'
-  import Prompt from '@/models/response/prompt'
-  import Question from '@/components/Question/Question.vue'
+<script setup lang="ts">
+import Welcome from '@/views/Welcome.vue'
+import Question from '@/components/Question.vue'
+import Finished from '@/views/Finished.vue'
+import useQuestionnaireStore from '@/stores/questionnaire'
+import { storeToRefs } from 'pinia'
+import { computed } from 'vue'
+import { find, isNil } from 'lodash-es'
+import surveyOrder from '@/data/surveyOrder'
 
-  /**
-   * Root container for the application. Displays the {@link Welcome} view if no surveys have been
-   * started, or the {@link Question} view if a survey is in progress, or the {@link Finished} view
-   * if all surveys are complete.
-   */
+/**
+ * Root container for the application. Displays the {@link Welcome} view if no surveys have been
+ * started, or the {@link Question} view if a survey is in progress, or the {@link Finished} view
+ * if all surveys are complete.
+ */
 
-  @Component({
-    components: { Question, Finished, Welcome },
-  })
-  export default class Container extends Vue {
-    @Getter clickedContinue!: boolean
+const store = useQuestionnaireStore()
+const { clickedContinue } = storeToRefs(store)
 
-    @Getter response!: (identifier: string) => Response
+const currentSurveyID = computed(() =>
+  find(surveyOrder, (ID) => !store.response(ID)?.isEffectivelyFinished)
+)
 
-    /**
-     * @return The next question to display to the user, or `undefined` if there are no more
-     * questions to ask, or the user hasn't started a survey yet.
-     */
+/**
+ * The next question to display to the user, or `undefined` if there are no more
+ * questions to ask, or the user hasn't started a survey yet.
+ */
+const nextPrompt = computed(() => {
+  if (!clickedContinue.value) return undefined
+  if (isNil(currentSurveyID.value)) return undefined
+  const prompt = store.response(currentSurveyID.value)?.nextQuestion
+  if (isNil(prompt)) return undefined
+  return prompt
+})
 
-    get nextPrompt(): Prompt | undefined {
-      if (!this.clickedContinue) return undefined
-      if (isNil(this.currentSurveyID)) return undefined
-      const prompt = this.response(this.currentSurveyID)?.nextQuestion
-      if (isNil(prompt)) return undefined
-      return prompt
-    }
+/** Whether to show the {@link Finished} view. */
+const showFinished = computed(() => clickedContinue.value && isNil(currentSurveyID.value))
 
-    /** @return Whether to show the {@link Finished} view. */
-    get showFinished(): boolean {
-      return this.clickedContinue && isNil(this.currentSurveyID)
-    }
+/** Whether to show the {@link Question} view. */
+const showQuestion = computed(() => clickedContinue.value && !isNil(currentSurveyID.value))
 
-    /** @return Whether to show the {@link Question} view. */
-    get showQuestion(): boolean {
-      return this.clickedContinue && !isNil(this.currentSurveyID)
-    }
-
-    /** @return Whether to show the {@link Welcome} view. */
-    get showWelcome(): boolean {
-      return !this.clickedContinue
-    }
-
-    private get currentSurveyID(): string | undefined {
-      return find(surveyOrder, (ID) => !this.response(ID)?.isEffectivelyFinished)
-    }
-  }
+/** Whether to show the {@link Welcome} view. */
+const showWelcome = computed(() => !clickedContinue.value)
 </script>
 
 <style lang="scss">
-  @use "src/assets/styles/responsive";
+@use '@/assets/styles/responsive';
 
-  #container {
-    @include responsive.fill-height;
+#container {
+  @include responsive.fill-height;
 
-    flex: 0 0 auto;
-    max-width: 800px;
-    width: 90vw;
+  flex: 0 0 auto;
+  width: 90vw;
+  max-width: 800px;
 
-    @include responsive.large {
-      margin-top: 10vh;
-      min-height: 90vh;
+  @include responsive.large {
+    min-height: 90vh;
+    margin-top: 10vh;
+  }
+
+  @include responsive.small {
+    min-height: 100vh;
+  }
+
+  // override the "move in" transition for the welcome page and have it fade in instead
+
+  #welcome {
+    &.in-move-left-out-move-left-enter-active {
+      transition-duration: 0.5s;
+      transition-property: opacity;
     }
 
-    @include responsive.small {
-      min-height: 100vh;
+    &.in-move-left-out-move-left-enter {
+      opacity: 0;
     }
 
-    // override the "move in" transition for the welcome page and have it fade in instead
-
-    #welcome {
-      &.in-move-left-out-move-left-enter-active {
-        transition-duration: 0.5s;
-        transition-property: opacity;
-      }
-
-      &.in-move-left-out-move-left-enter {
-        opacity: 0;
-      }
-
-      &.in-move-left-out-move-left-enter-to {
-        opacity: 1;
-      }
+    &.in-move-left-out-move-left-enter-to {
+      opacity: 1;
     }
   }
+}
 </style>
